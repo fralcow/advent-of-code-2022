a2f0a4cbd98803f61af14e135dbd06e6c434a9ee
Andrey Krupskiy <andrey.krupskiy@and.digital>
2022-12-06 16:16:49 +0100

    day 4 star 2

--- day_4/src/main.rs
+++ day_4/src/main.rs
@@ -45,6 +45,7 @@ fn main() {
         .map(|v| v.unwrap())
         .collect();
 
+    // problem 1
     let number_contains_whole: usize = pairs_range
         .iter()
         .cloned()
@@ -66,6 +67,35 @@ fn main() {
         .sum();
 
     println!("number_contains_whole: {:?}", number_contains_whole);
+
+    // problem 2
+    let number_contains_partial: usize = pairs_range
+        .iter()
+        .map(|pair| {
+            let left_range = &pair.0;
+            let right_range = &pair.1;
+
+            let mut to_return: Option<usize> = Some(0);
+
+            // left_start .. right_start .. left_end
+            if (left_range.start <= right_range.start && right_range.start <= left_range.end) ||
+            // left_start .. right_end .. left_end
+             (left_range.start <= right_range.end && right_range.end <= left_range.end) ||
+            // right_start .. left_start .. right_end
+             (right_range.start <= left_range.start && left_range.start <= right_range.end) ||
+            // right_start .. left_end .. right_end
+             (right_range.start <= left_range.end && left_range.end <= right_range.end)
+            {
+                to_return = Some(1);
+            };
+
+            return to_return;
+        })
+        .filter(|v| v.is_some())
+        .map(|v| v.unwrap())
+        .sum();
+
+    println!("number_contains_partial: {:?}", number_contains_partial);
 }
 fn parse_range(range: &str) -> Result<Range<usize>, &'static str> {
     let (start, end) = match range.split_once('-') {
