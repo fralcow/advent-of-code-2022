fd99b380c79505d993c18f3c4bf9a7c36ba01b66
krup <krupskiy.andrey@gmail.com>
2022-12-07 22:52:54 +0100

    day 6 star 2

--- day_6/src/main.rs
+++ day_6/src/main.rs
@@ -16,20 +16,29 @@ fn main() {
 
     let input: Vec<char> = input.chars().collect();
 
-    let mut index = 0;
-    for w in input.windows(4) {
+    // problem 1
+    let problem_1_answer = find_marker_index(input.clone(), 4);
+    println!("problem_1_answer: {:?}", problem_1_answer);
+
+    // problem_2_answer
+    let problem_2_answer = find_marker_index(input, 14);
+    println!("problem_2_answer: {:?}", problem_2_answer);
+}
+
+fn find_marker_index(input: Vec<char>, window_size: usize) -> usize {
+    let mut index: usize = 0;
+    for w in input.windows(window_size) {
         let mut set: HashSet<char> = HashSet::new();
 
         w.iter().for_each(|&letter| {
             _ = set.insert(letter);
         });
 
-        if set.len() == 4 {
-            println!("index: {:?}", index + 4); // + 4 because we're looking for the index of the
-                                                // last marker character
-            return;
+        if set.len() == window_size {
+            return index + window_size;
         }
 
         index += 1;
     }
+    return 0;
 }
