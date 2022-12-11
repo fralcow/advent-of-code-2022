a0f8977b2217910abf4754cde12545136df9f45f
krup <krupskiy.andrey@gmail.com>
2022-12-12 00:35:17 +0100

    D9: rope creation and moving by head

---
+++ day_9/src/main.rs
@@ -0,0 +1,203 @@
+mod input;
+use std::fmt::Display;
+
+#[derive(Copy, Clone)]
+enum Direction {
+    Up,
+    UpRight,
+    Right,
+    DownRight,
+    Down,
+    DownLeft,
+    Left,
+    UpLeft,
+}
+
+#[derive(Copy, Clone, Debug)]
+struct Cell {
+    x: i32,
+    y: i32,
+}
+
+impl Cell {
+    fn new(x: i32, y: i32) -> Self {
+        return Cell { x, y };
+    }
+
+    fn touches(&self, other_cell: Cell) -> bool {
+        if i32::abs(self.x - other_cell.x) <= 1 && i32::abs(self.y - other_cell.y) <= 1 {
+            return true;
+        }
+        return false;
+    }
+}
+
+impl PartialEq for Cell {
+    fn eq(&self, other: &Self) -> bool {
+        if self.x == other.x && self.y == other.y {
+            return true;
+        }
+        return false;
+    }
+}
+
+impl Display for Cell {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        return write!(f, "[{},{}]", self.x, self.y);
+    }
+}
+
+#[test]
+fn touches_test() {
+    let cell_0 = Cell::new(0, 0);
+    assert!(cell_0.touches(Cell::new(0, 0)));
+    assert!(cell_0.touches(Cell::new(1, 1)));
+    assert!(!cell_0.touches(Cell::new(2, 1)));
+}
+
+struct Rope {
+    head: RopeEnd,
+    tail: RopeEnd,
+}
+
+impl Rope {
+    fn new() -> Self {
+        return Rope {
+            head: RopeEnd::new(0, 0),
+            tail: RopeEnd::new(0, 0),
+        };
+    }
+
+    fn move_by_head(&mut self, direction: Direction) {
+        // move head
+        self.head.move_command(direction.clone());
+
+        // check if tail still touches the head
+        if self.head.cell.touches(self.tail.cell) {
+            return;
+        }
+
+        // // if same column/row -> move in same direction as head
+        // move tail
+        if self.tail.cell.x == self.head.cell.x || self.tail.cell.y == self.head.cell.y {
+            self.tail.move_command(direction.clone());
+        } else {
+            // // if in different column -> move to the cell with the same x or y
+            match direction {
+                Direction::Up => {
+                    if self.tail.cell.x - self.head.cell.x < 0 {
+                        self.tail.move_command(Direction::UpRight);
+                    } else {
+                        self.tail.move_command(Direction::UpLeft);
+                    }
+                }
+                Direction::Right => {
+                    if self.tail.cell.y - self.head.cell.y < 0 {
+                        self.tail.move_command(Direction::UpRight);
+                    } else {
+                        self.tail.move_command(Direction::DownRight);
+                    }
+                }
+                Direction::Down => {
+                    if self.tail.cell.x - self.head.cell.x < 0 {
+                        self.tail.move_command(Direction::DownRight);
+                    } else {
+                        self.tail.move_command(Direction::DownLeft);
+                    }
+                }
+                Direction::Left => {
+                    if self.tail.cell.y - self.head.cell.y < 0 {
+                        self.tail.move_command(Direction::UpLeft);
+                    } else {
+                        self.tail.move_command(Direction::DownLeft);
+                    }
+                }
+                _ => panic!("Unexpected head direction command"),
+            }
+        }
+
+        // // add the tail cell to the tail visited vector
+        self.tail.visitedCells.push(self.tail.cell.clone());
+        return;
+    }
+}
+
+#[test]
+fn test_move_rope_by_head() {
+    let mut r = Rope::new();
+    assert_eq!(r.tail.visitedCells[0], Cell::new(0, 0));
+
+    r.move_by_head(Direction::Up); // 0,1
+    assert_eq!(r.tail.cell, Cell::new(0, 0));
+
+    r.move_by_head(Direction::Up); // 0,2
+    assert_eq!(r.tail.cell, Cell::new(0, 1));
+    assert_eq!(r.tail.visitedCells[1], Cell::new(0, 1));
+
+    r.move_by_head(Direction::Right); // 1,2
+    assert_eq!(r.tail.cell, Cell::new(0, 1));
+
+    r.move_by_head(Direction::Up); // 1,3
+    assert_eq!(r.tail.cell, Cell::new(1, 2));
+    assert_eq!(r.tail.visitedCells[2], Cell::new(1, 2));
+}
+
+impl Display for Rope {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        return write!(f, "Head: {}\nTail: {}", self.head.cell, self.tail.cell);
+    }
+}
+
+struct RopeEnd {
+    cell: Cell,
+    visitedCells: Vec<Cell>,
+}
+
+impl RopeEnd {
+    fn new(x: i32, y: i32) -> Self {
+        return RopeEnd {
+            cell: Cell::new(x, y),
+            visitedCells: vec![Cell::new(x, y)],
+        };
+    }
+
+    fn move_command(&mut self, direction: Direction) {
+        match direction {
+            Direction::Up => self.cell.y += 1,
+            Direction::UpRight => {
+                self.cell.x += 1;
+                self.cell.y += 1
+            }
+            Direction::Right => self.cell.x += 1,
+            Direction::DownRight => {
+                self.cell.x += 1;
+                self.cell.y -= 1
+            }
+            Direction::Down => self.cell.y -= 1,
+            Direction::DownLeft => {
+                self.cell.x -= 1;
+                self.cell.y -= 1
+            }
+            Direction::Left => self.cell.x -= 1,
+            Direction::UpLeft => {
+                self.cell.x -= 1;
+                self.cell.y += 1
+            }
+        }
+    }
+}
+
+impl Display for RopeEnd {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        return write!(f, "{}", self.cell);
+    }
+}
+
+fn main() {
+    let mut r = Rope::new();
+    r.move_by_head(Direction::Up);
+    println!("rope:\n{}", r);
+
+    r.move_by_head(Direction::Up);
+    println!("rope:\n{}", r);
+}
